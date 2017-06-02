97ddb47831247b2d89dcd41342047edcadb5a78e
Trent Mentink <trentmentink@gmail.com>
2017-06-02 11:16:36 -0700

    updated dependencies. updated test folder. updated eslint rules.

--- gruntfile.js
+++ gruntfile.js
@@ -7,23 +7,31 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    version: "<%= pkg.version %>-alpha",
     banner: "/*!\n" +
-                " * <%= pkg.name %> v<%= pkg.version %> (<%= pkg.homepage %>)\n" +
+                " * <%= pkg.name %> v<%= version %> (<%= pkg.homepage %>)\n" +
                 " * Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author %>\n" +
                 " * Licensed under <%= pkg.license %>\n" +
                 " */\n",
+    dependencyCheck: "",
+
+
+    // --------------------------------------------------------------------
+    // Grunt Tasks
+    // --------------------------------------------------------------------
+
     babel: {
       js: {
         files: {
-          "<%= concat.js.dest %>" : "<%= concat.js.dest %>"
+          "<%= concat.project.dest %>" : "<%= concat.project.dest %>"
         }
       }
     },
     clean: {
-      js: ["<%= concat.js.dest %>"]
+      js: ["<%= concat.project.dest %>"]
     },
     concat: {
-      js: {
+      project: {
         src: ["src/js/vendor/jquery-shim.js"],
         dest: "src/js/main.js"
       }
@@ -31,33 +39,43 @@ module.exports = function(grunt) {
     eslint: {
       target: ["src/js/**/*.js"]
     },
+    stamp: {
+      project: {
+        options: {
+          banner: "<%= dependencyCheck %>"
+        },
+        files: {
+          src: "<%= concat.project.dest %>"
+        }
+      }
+    },
     uglify: {
       dev: {
         options: {
           banner: "<%= banner %>",
-          beautify: {
-            beautify: true,
-            indent_level: 2,
-          },
+          beautify: true,
           compress: false,
           mangle: false,
-          preserveComments: /\*/
+          output: {
+            indent_level: 2,
+            comments: /\*/
+          }
         },
-        src: "<%= concat.js.dest %>",
+        src: "<%= concat.project.dest %>",
         dest: "dist/<%= pkg.name %>.js",
       },
       dist: {
         options: {
           banner: "<%= banner %>"
         },
-        src: "<%= concat.js.dest %>",
+        src: "<%= concat.project.dest %>",
         dest: "dist/<%= pkg.name %>.min.js",
       }
     },
     watch: {
       js: {
         files: ["src/js/**/*.js"],
-        tasks: ["concat", "babel", "uglify:dev"]
+        tasks: ["concat", "stamp", "babel", "uglify:dev", "clean"]
       }
     }
   })
@@ -65,6 +83,6 @@ module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt)
   require("time-grunt")(grunt)
 
-  grunt.registerTask("default", ["eslint", "concat", "babel", "uglify", "clean"])
+  grunt.registerTask("default", ["eslint", "concat", "stamp", "babel", "uglify", "clean"])
   grunt.registerTask("lint", ["eslint"])
 }
